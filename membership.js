28e7232d8bdf00da3cab021cb833aa0c14d56529
Ashraful Islam <90891049+ashraful-islam-siam@users.noreply.github.com>
2024-09-02 01:19:25 +0600

    Update membership.js

--- membership.js
+++ membership.js
@@ -1,6 +1,6 @@
 const books = ["The art of thinking clearly", "Javascript", "Road To Success"];
 
-if (books.includes("Javascript")) {
+if (books.includes("Javascript") === true) {
   console.log("We have found the book");
 } else {
   console.log("Sorry, search a authentic book");
